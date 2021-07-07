cfd39424e2ea6f02574c1eac6ad9449b3fde7df5
13731160065 <714318973@qq.com>
2021-07-07 22:07:58 +0800

    xx

---
+++ WZZDataTableViewDemo/WZZDataTableViewDemo/TestContentCell.h
@@ -0,0 +1,16 @@
+//
+//  TestContentCell.h
+//  WZZDataTableViewDemo
+//
+//  Created by 王泽众 on 2021/7/7.
+//
+
+#import "WZZDataTableViewCell.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestContentCell : WZZDataTableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WZZDataTableViewDemo/WZZDataTableViewDemo/TestHeadCell.h
@@ -0,0 +1,16 @@
+//
+//  TestHeadCell.h
+//  WZZDataTableViewDemo
+//
+//  Created by 王泽众 on 2021/7/7.
+//
+
+#import "WZZDataTableViewCell.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestHeadCell : WZZDataTableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
